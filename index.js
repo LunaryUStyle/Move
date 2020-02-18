6447f5b9d005b59483eb22745cc74c8ee01ca814
LunaryUStyle <45127299+LunaryUStyle@users.noreply.github.com>
2020-02-18 11:26:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 
-client.login(NjcxNzQ3MzUzNjYwODgyOTQ0.XkppUQ.zhpX1LAI-hTKpkCSBMjE66TWZW4);
+client.login(process.env.TOKEN);
 
 client.commands = new Discord.Collection();
 
